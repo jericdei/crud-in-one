c0b5e34d9b441d50952180df95c574a1cc24bc92
Jeric <jericjune.logan.lhoopa@gmail.com>
2023-08-17 23:25:22 +0800

    react setup

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    https: false,
+    host: true,
+    strictPort: true,
+    port: 5173,
+    hmr: {
+        host: "localhost",
+        protocol: "ws",
+    },
+    watch: {
+        usePolling: true,
+    },
+},
 })
